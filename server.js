68ae0e57df5d9704c31f71ad9c9abe9b8f2ef73a
Naveedahmedtech <technaveedahmed@gmail.com>
2024-09-28 15:12:45 +0500

    updated

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Listing to ${PORT}`))
